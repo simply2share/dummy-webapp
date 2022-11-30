c3cad791c90821b14261a69fb43ffe3752c8e0ce
zlodij <ya-zlodij@yandex.com>
2022-11-30 13:32:27 +0200

    Initial commit

---
+++ dummy-webapp/src/main/java/ua/jdev/DummyWebApp.java
@@ -0,0 +1,13 @@
+package ua.jdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyWebApp.class, args);
+	}
+
+}
---
+++ dummy-webapp/src/test/java/ua/jdev/DummyWebAppTests.java
@@ -0,0 +1,13 @@
+package ua.jdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
