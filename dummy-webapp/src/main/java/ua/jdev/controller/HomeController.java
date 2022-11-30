dfac58374fe02e94146b7cac60af1a7e5e7786a7
zlodij <ya-zlodij@yandex.com>
2022-11-30 16:19:54 +0200

    Add like/dislike counting

--- dummy-webapp/src/main/java/ua/jdev/controller/HomeController.java
+++ dummy-webapp/src/main/java/ua/jdev/controller/HomeController.java
@@ -1,12 +1,34 @@
 package ua.jdev.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	private int likes = 0;
+	private int dislikes = 0;
+	
 	@GetMapping({ "/", "/home" })
-	public String home() {
+	public String home(Model model) {
+		
+		model.addAttribute("likes", likes);
+		model.addAttribute("dislikes", dislikes);
+		
 		return "index";
+	}
+	
+	@GetMapping("/like")
+	public String like(Model model) {
+		likes++;
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/dislike")
+	public String dislike(Model model) {
+		dislikes++;
+		
+		return "redirect:/";
 	}
 }
