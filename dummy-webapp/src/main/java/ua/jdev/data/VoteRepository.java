df8e762d4aac3f4a05a783e17f096f19cef61d89
zlodij <ya-zlodij@yandex.com>
2022-12-01 22:42:53 +0200

    Initial commit

---
+++ dummy-webapp/src/main/java/ua/jdev/data/VoteRepository.java
@@ -0,0 +1,13 @@
+package ua.jdev.data;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface VoteRepository extends CrudRepository<Vote, Long> {
+	@Query(value = "select count(*) as total, "
+			+ "	 ifnull(sum(case when decision=true then 1 else 0 end),0) as likes, "
+			+ "	 ifnull(sum(case when decision=false then 1 else 0 end),0) as dislikes from vote")
+	Statistics getStatistics();
+}
