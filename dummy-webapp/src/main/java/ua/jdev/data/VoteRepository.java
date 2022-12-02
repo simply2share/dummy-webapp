4de4c021d6dec807dcf80ae84b6b7a77010681f1
zlodij <ya-zlodij@yandex.com>
2022-12-02 21:37:39 +0200

    Change SQL-query syntax from H2 to Postgres-specific

--- dummy-webapp/src/main/java/ua/jdev/data/VoteRepository.java
+++ dummy-webapp/src/main/java/ua/jdev/data/VoteRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 interface VoteRepository extends CrudRepository<Vote, Long> {
 	@Query(value = "select count(*) as total, "
-			+ "	 ifnull(sum(case when decision=true then 1 else 0 end),0) as likes, "
-			+ "	 ifnull(sum(case when decision=false then 1 else 0 end),0) as dislikes from vote")
+			+ "	 coalesce(sum(case when decision=true then 1 else 0 end),0) as likes, "
+			+ "	 coalesce(sum(case when decision=false then 1 else 0 end),0) as dislikes from vote")
 	Statistics getStatistics();
 }
