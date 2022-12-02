e82a6eabadc2248057825e39f1fe0a72358abf16
zlodij <ya-zlodij@yandex.com>
2022-12-02 21:35:37 +0200

    Remove redundant annotation

--- dummy-webapp/src/main/java/ua/jdev/data/Vote.java
+++ dummy-webapp/src/main/java/ua/jdev/data/Vote.java
@@ -3,8 +3,6 @@ package ua.jdev.data;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
-@Table("VOTE")
 record Vote(@Id Long id, Boolean decision, Date voteDate) {
 }
